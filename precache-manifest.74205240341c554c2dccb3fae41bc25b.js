65f7b37121fdb6f6121804aed20ff1d3140c6e84
Benjamin Wang <crocodile00323@gmail.com>
2019-08-02 09:29:46 +0800

    Updates

---
+++ precache-manifest.74205240341c554c2dccb3fae41bc25b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2632edcbeacdd3861e423317a00e4c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "12e4dc9097daf1ab4911",
+    "url": "/robofriends/static/css/2.25513fc0.chunk.css"
+  },
+  {
+    "revision": "44f41ee5d5705e3048f7",
+    "url": "/robofriends/static/css/main.33adfb75.chunk.css"
+  },
+  {
+    "revision": "12e4dc9097daf1ab4911",
+    "url": "/robofriends/static/js/2.d5a06358.chunk.js"
+  },
+  {
+    "revision": "94a4ddc6c4cb4ac25a42",
+    "url": "/robofriends/static/js/3.806cadb5.chunk.js"
+  },
+  {
+    "revision": "44f41ee5d5705e3048f7",
+    "url": "/robofriends/static/js/main.31b2d8c9.chunk.js"
+  },
+  {
+    "revision": "1f29eb3e55b71c2140ec",
+    "url": "/robofriends/static/js/runtime~main.d2953aa1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
